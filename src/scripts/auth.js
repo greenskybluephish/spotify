de4d21b98c8546760f1d995494d63d79be4cac03
greenskybluephish <greenskybluephish@gmail.com>
2019-03-07 16:28:44 -0600

    fixed file paths

---
+++ src/scripts/auth.js
@@ -0,0 +1,28 @@
+const hash = window.location.hash
+.substring(1)
+.split('&')
+.reduce(function (initial, item) {
+  if (item) {
+    var parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+  }
+  return initial;
+}, {});
+window.location.hash = '';
+
+// Set token
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = '80c3cf7bdf244f508df3080b2acd8696';
+const redirectUri = 'http://localhost:8080/';
+const scopes = [
+  'user-top-read'
+];
+
+// If there is no token, redirect to Spotify authorization
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&response_type=token&show_dialog=true`;
+}
